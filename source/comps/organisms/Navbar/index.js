cdeb8c42e9b77f8a6f88340fa170aa12e88ca08f
Colton Colcleasure <colshacol@gmail.com>
2017-10-08 06:24:00 -0500

    Added basic Navbar stuffs

--- source/comps/organisms/Navbar/index.js
+++ source/comps/organisms/Navbar/index.js
@@ -1,1 +1,25 @@
-export Navbar from './Navbar'
+import { connect } from 'react-redux'
+
+import Navbar from './Navbar'
+import * as userActions from '@store/actions/users'
+
+const mapStateToProps = state => {
+	return {
+		session: state.session
+	}
+}
+
+const mapActionsToProps = dispatch => {
+	return {
+		actions: {
+			logHello() {
+				console.log('hello, fucker.')
+			}
+		}
+	}
+}
+
+export default connect(
+	mapStateToProps,
+	mapActionsToProps
+)(Navbar)
