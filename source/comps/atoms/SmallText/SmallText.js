59a1e178a23119eed0c814b87cf1724dc2766992
Colton Colcleasure <colton.colcleasure.ctr@sabre.com>
2017-10-10 02:50:42 -0500

    Added more components.

---
+++ source/comps/atoms/SmallText/SmallText.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/SmallText.css'
+
+const SmallText = (props: PropsT) => {
+	return (
+		<div styleName='SmallText'>
+
+		</div>
+	)
+}
+
+export default SmallText
--- source/comps/atoms/index.js
+++ source/comps/atoms/index.js
@@ -1,4 +1,8 @@
 export { Title } from './Title';
 export { Input } from './Input';
 export { InlineButton } from './InlineButton';
-export { InputBox } from './InputBox';
+export { HorizontalLine } from './HorizontalLine';
+export { Icon } from './Icon';
+export { SmallText } from './SmallText';
+export { Paragraph } from './Paragraph';
+
