ab4ee2260d25b503e4f711ea16438cd72f0dec3d
ClintonBaker <c.dillon.baker@gmail.com>
2017-10-13 03:02:39 -0500

    Linked Submit scene to store and cleaned up a bit

--- source/comps/scenes/Submit/index.js
+++ source/comps/scenes/Submit/index.js
@@ -1,22 +1,16 @@
 import { connect } from 'react-redux';
 import Submit from './Submit';
+import { postThing } from '@store/actions/things';
+import { getUsers } from '@store/actions/users';
 
 const mapStateToProps = state => {
-	// console.log(state);
+	
 	return {
 		FormPosted: state.things.FormPosted,
 		ID: state.things.ID
 	};
 };
 
-// const mapActionsToProps = dispatch => {
-// 	return {
-// 		actions: {
-//       postThing( form ) {
-//         dispatch(postThing( form ));
-// 			}
-// 		}
-// 	}
-// }
-
-export default connect(mapStateToProps)(Submit);
+export default connect(
+	mapStateToProps
+)(Submit)
