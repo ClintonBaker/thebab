c11be2ffe8cd0f48e455cc040882f6e51058be85
ClintonBaker <c.dillon.baker@gmail.com>
2017-10-20 17:40:52 -0500

    Weird things are happening here...

--- source/comps/NewUserForm/tests/NewUserForm.test.js
+++ source/comps/NewUserForm/tests/NewUserForm.test.js
@@ -1,7 +1,7 @@
 import ava from 'ava';
 import React from 'react';
 import { shallow } from 'enzyme';
-import NewUserForm from '../NewUserForm';
+import NewUserForm from '../';
 
 // console.log('NewUserForm');
 ava.test.todo('- write tests for NewUserForm.');
