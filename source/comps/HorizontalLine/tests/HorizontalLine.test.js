43147f027fb2b83a57bb04bbecea1c4813864881
ClintonBaker <c.dillon.baker@gmail.com>
2017-11-03 01:12:27 -0500

    Fixed some of the packages

--- source/comps/HorizontalLine/tests/HorizontalLine.test.js
+++ source/comps/HorizontalLine/tests/HorizontalLine.test.js
@@ -1,7 +1,7 @@
 import ava from 'ava';
 import React from 'react';
 import { shallow } from 'enzyme';
-import HorizontalLine from '../HorizontalLine';
+import HorizontalLine from '../';
 
 // console.log('HorizontalLine');
 ava.test.todo('- write tests for HorizontalLine.');
--- source/comps/Input/tests/Input.test.js
+++ source/comps/Input/tests/Input.test.js
@@ -1,7 +1,7 @@
 import ava from 'ava';
 import React from 'react';
 import { shallow } from 'enzyme';
-import Input from '../Input';
+import Input from '../';
 
 // console.log('Input');
 ava.test.todo('- write tests for Input.');
